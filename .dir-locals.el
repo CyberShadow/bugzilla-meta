9bfadd4a89ec37bd5f598221e6403615f7d46597
Vladimir Panteleev <git@thecybershadow.net>
2020-06-24 20:08:20 +0000

    .dir-locals.el: Add perl-mode settings; reformat

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,11 @@
-((web-mode
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((perl-mode
+  (tab-width . 2)
+  (perl-indent-level . 2))
+ (web-mode
   (web-mode-markup-indent-offset . 2)
-  (indent-tabs-mode . nil)
-  (smart-tabs-mode . nil)
-  (bug-reference-url-format . "https://bugzilla.mozilla.org/show_bug.cgi?id=%s")
-  ))
+  (indent-tabs-mode)
+  (smart-tabs-mode)
+  (bug-reference-url-format . "https://bugzilla.mozilla.org/show_bug.cgi?id=%s")))
