4be8e2439e8ceb86797dcc701deda6433d27302d
Vladimir Panteleev <git@thecybershadow.net>
2020-10-08 23:42:44 +0000

    .dir-locals.el: Configure flycheck-perl-include-path

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,12 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((perl-mode . ((tab-width . 2)
+	       (eval . (setq flycheck-perl-include-path
+			     (list
+			      (expand-file-name "src"
+						(locate-dominating-file default-directory ".dir-locals.el"))
+			      (expand-file-name "src/local/lib/perl5"
+						(locate-dominating-file default-directory ".dir-locals.el")))))
 	       (perl-indent-level . 2)))
  (web-mode . ((web-mode-markup-indent-offset . 2)
 	      (indent-tabs-mode . nil)
